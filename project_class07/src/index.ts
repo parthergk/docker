ee05f1cdc2cdbfa8b25524168a7f2d4344028f60
parthergk <gauravkumar81464@gmail.com>
2025-04-24 13:51:03 +0530

    class 07

---
+++ project_class07/src/index.ts
@@ -0,0 +1,23 @@
+import  Express, { Request, Response }  from "express";
+import { PrismaClient } from "../generated/prisma";
+const app = Express();
+const prismaClient = new PrismaClient();
+
+app.get('/', async(req:Request, res:Response)=>{
+    const user = await prismaClient.user.findMany();
+    res.json({message:"data from the db", user})
+})
+
+app.post('/', async(req:Request,res:Response)=>{
+    await prismaClient.user.create({
+        data:{
+            username:"Gaurav Kumar",
+            password: "Gaurav"
+        }
+    })
+})
+
+app.listen(3000, ()=>{
+    console.log("http server is runing on port 3000");
+    
+})
